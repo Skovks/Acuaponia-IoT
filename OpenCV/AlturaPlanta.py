f8c8bd16cc6f7b4eec1dced1fa86b371b7750f83
Skovks <al2152000114@azc.uam.mx>
2022-02-23 03:32:10 -0600

    Pruebas de medicion con objetos
    Fotos de ejemplo y script de python para la medicion de altura de la planta

---
+++ OpenCV/AlturaPlanta.py
@@ -0,0 +1,82 @@
+import imutils.contours
+import cv2 
+import cv2 as cv
+
+image = cv.imread('Fotos/flower1.jpg')
+#cv.imshow('Oaxaca', image)
+
+# Cover to grayscale and blur
+greyscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+greyscale = cv2.GaussianBlur(greyscale, (7, 7), 0)
+ret, thresh=cv.threshold(greyscale, 50, 255, cv.THRESH_BINARY)
+# Resize and display the image (press key to exit)
+resized_image2 = cv2.resize(thresh, (1280, 720))
+cv2.imshow("Imagen binarizada", resized_image2)
+
+
+# Detect edges and close gaps
+canny_output = cv2.Canny(thresh, 50, 100)
+# Resize and display the image (press key to exit)
+resized_image3 = cv2.resize(canny_output, (1280, 720))
+cv2.imshow("Imagen con canny", resized_image3)
+canny_output = cv2.dilate(canny_output, None, iterations=1)
+# Resize and display the image (press key to exit)
+resized_image4 = cv2.resize(canny_output, (1280, 720))
+cv2.imshow("Imagen dilatada", resized_image4)
+
+
+# Get the contours of the shapes, sort l-to-r and create boxes
+contours, hierarchies = cv2.findContours(canny_output, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+if len(contours) < 2:
+    print("Couldn't detect two or more objects")
+    exit(0)
+
+(contours, _) = imutils.contours.sort_contours(contours)
+contours_poly = [None]*len(contours)
+boundRect = [None]*len(contours)
+for i, c in enumerate(contours):
+    contours_poly[i] = cv2.approxPolyDP(c, 3, True)
+    boundRect[i] = cv2.boundingRect(contours_poly[i])
+
+output_image = image.copy()
+mmPerPixel = 16 / boundRect[0][2]
+print(boundRect[0][2])
+print(mmPerPixel)
+highestRect = 1000
+lowestRect = 0
+for i in range(0, len(contours)):
+
+    # Too smol?
+    if boundRect[i][2] < 100 or boundRect[i][3] < 100:
+        continue
+
+    # The first rectangle is our control, so set the ratio
+    if highestRect > boundRect[i][1]:
+        highestRect = boundRect[i][1]
+    if lowestRect < (boundRect[i][1] + boundRect[i][3]):
+        lowestRect = (boundRect[i][1] + boundRect[i][3])
+
+    # Create a boundary box
+    cv2.rectangle(output_image, (int(boundRect[i][0]), int(boundRect[i][1])),
+                  (int(boundRect[i][0] + boundRect[i][2]),
+                  int(boundRect[i][1] + boundRect[i][3])), (0, 255, 0), 3)
+    print(boundRect[i],i)
+PrimerRect = image.copy()
+# Create a boundary box
+cv2.rectangle(PrimerRect, (int(boundRect[0][0]), int(boundRect[0][1])),(int(boundRect[0][0] + boundRect[0][2]), int(boundRect[0][1] + boundRect[0][3])), (0, 255, 0), 3)
+# Resize and display the image (press key to exit)
+resized_imagePrimerRec = cv2.resize(PrimerRect, (1280, 720))
+cv2.imshow("Primer rectangulo", resized_imagePrimerRec)
+
+
+print(lowestRect)
+print(highestRect)
+# Calculate the size of our plant
+plantHeight = (lowestRect - highestRect) * mmPerPixel
+print("Plant height is {0:.0f}mm".format(plantHeight))
+
+# Resize and display the image (press key to exit)
+resized_image = cv2.resize(output_image, (1280, 720))
+cv2.imshow("Image", resized_image)
+cv2.waitKey(0)
