a498de8d5c6d04d3093a4a8f3424e23839d337e1
Skovks <al2152000114@azc.uam.mx>
2022-06-21 18:29:17 -0500

    Comentarios de descripcion al codigo

--- Acoples/E1E2bombas.py
+++ Acoples/E1E2bombas.py
@@ -2,49 +2,58 @@
    *Autor: Skovks
    *
    *Este programa sirve para el control de de llenado de una pecera
-   *con su deposito de agua a travez de 2 sensores flotadores sencillos
-   *y 1 flotador doble.
-   *Flotadores verticales 1 es abajo y 0 arriba
-   *Flotador horizontal 1 es arriba y 0 es abajo
-   *relay 1 es apagado y 0 es prendido
+   *con su deposito de agua a travez de 2 sensores flotadores dobles
+   *Flotadores convencion de funcionamiento para este programa
+   *Cuando en el sofware sea '1' digital indicara que el flotador esta abajo(No detecta agua) 
+   *Cuando en el sofware sea '0' digital indicara que el flotador esta arriba(Detecta agua) 
+   *Cuando a el relay se le envie un '1' digital este estara apagado(Bomba apagada)
+   *Cuando a el relay se le envie un '0' digital este estara prendido(Bomba encendida)
    *
    '''
 #Bibliotecas   
-import RPi.GPIO as GPIO
-import time
-from datetime import datetime
-import psycopg2 #biblioteca para base de datos postgresql
+import RPi.GPIO as GPIO #Comunicacion para los pines GPIO
+import time #Para uso de la funcion sleep
+from datetime import datetime #Acceso a la hora actual
+import psycopg2 #Comunicacion para base de datos postgresql
 
 
 #Constantes 
-GPIO.setmode(GPIO.BOARD)
-FFL = 29 #Flotador del Filtro Nivel Low
-FFH = 31 #Flotador del Filtro Nivel High
-FPL = 36 #Flotador de la Pecera Nivel Low
-FPH = 38 #Flotador de la Pecera Nivel High
-RB1 = 15 #Relay de la bomba de deposito
-RB2 = 19 # Relay de la bomba de pecera
-RL = 40 #Relay de Luces  
-bf=0 #bandera de inicio bombas
-bl=0 #bandera de inicio de luces
+GPIO.setmode(GPIO.BOARD) #Funcion para usar los pines por orden de numeracion(1-40)
+FFL = 29 #Flotador del Filtro para el Nivel Bajo 
+FFH = 31 #Flotador del Filtro para el Nivel Alto
+FPL = 36 #Flotador de la Pecera para el Nivel Bajo
+FPH = 38 #Flotador de la Pecera para el Nivel Alto
+RB1 = 15 #Relay para prender/apagar la bomba del deposito
+RB2 = 19 #Relay para prender/apagar la bomba de la pecera
+RL = 40 #Relay para el encendido/apagado de Luces  
+bf=0 #bandera de inicio para bombas
+bl=0 #bandera de inicio para luces
 
 #Cuerpo del programa
 #Inicializamos los pines de entrada y salida
-GPIO.setup(FFL, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(FFL, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #GPIO.setup(sensor,Entrada, resistencia interna en modo pull down)
 GPIO.setup(FFH, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(FPL, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(FPH, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(RB1, GPIO.OUT, initial = 1)
+GPIO.setup(RB1, GPIO.OUT, initial = 1) #GPIO.setup(actuador, Salida, condicion inicial)
 GPIO.setup(RB2, GPIO.OUT, initial = 1)
 GPIO.setup(RL, GPIO.OUT, initial = 0)
 
-#Bucle infinito
-try:
+#Inicio del programa Principal
+try: # 
+	#Conexion a la base de datos de POSTGRESQL para leer datos
+	#Primer parametro host=(IP de la base de datos o en su defecto localhost)
+	#Segundo parametro database=(nombre de la base de datos)
+	#Tercer parametro user=(usuario que tenga acceso a la base datos seleccionada)
+	#Cuarto párametro password=(Contraseña del usuario) 
 	conexion = psycopg2.connect(host="localhost", database="acuaponia", user="grafana", password="1234")
-	cur = conexion.cursor()
-	while True:
+	cur = conexion.cursor() #Funcion para poder accesar a los datos
+	while True: #Bucle infinito
+		'''En la siguiente linea de codigo se especifican los datos a los cuales se quiere acceder a traves de la tabla
+		con los ultimos registros guardados. Lo que esta dentro de la funcion es una cadena de caracteres con una 
+		sentencia SQL para acceder a los datos solicitados'''
 		cur.execute("SELECT hora_apagado, hora_encendido, minuto_apagado, minuto_encendido FROM public.\"controlLuces_programacion_luces\" WHERE id=(select max(id) from public.\"controlLuces_programacion_luces\")")
-		for x in cur.fetchall():
+		for x in cur.fetchall(): #
 			hora_apagado=x[0] 
 			hora_encendido=x[1] 
 			minuto_apagado=x[2]
